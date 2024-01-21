dc1910bfed4fee383d536f5da93600f84ee3a19d
Eilliyah Fong Binti Abdullah 300157 <eilliyah_fong_a@sqs.uum.edu.my>
2024-01-21 23:11:09 +0800

    loginnnnnn

---
+++ My Project/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtUsername = New System.Windows.Forms.TextBox()
+        Me.TxtPIN = New System.Windows.Forms.TextBox()
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.LblSignup = New System.Windows.Forms.Label()
+        Me.LblUsername = New System.Windows.Forms.Label()
+        Me.LblPIN = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Location = New System.Drawing.Point(146, 87)
+        Me.TxtUsername.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(184, 22)
+        Me.TxtUsername.TabIndex = 0
+        '
+        'TxtPIN
+        '
+        Me.TxtPIN.Location = New System.Drawing.Point(146, 113)
+        Me.TxtPIN.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TxtPIN.Name = "TxtPIN"
+        Me.TxtPIN.Size = New System.Drawing.Size(184, 22)
+        Me.TxtPIN.TabIndex = 1
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnLogin.Location = New System.Drawing.Point(164, 163)
+        Me.BtnLogin.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(109, 57)
+        Me.BtnLogin.TabIndex = 2
+        Me.BtnLogin.Text = "Log In"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'LblSignup
+        '
+        Me.LblSignup.AutoSize = True
+        Me.LblSignup.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblSignup.Location = New System.Drawing.Point(342, 163)
+        Me.LblSignup.Name = "LblSignup"
+        Me.LblSignup.Size = New System.Drawing.Size(115, 32)
+        Me.LblSignup.TabIndex = 3
+        Me.LblSignup.Text = "Sign Up"
+        '
+        'LblUsername
+        '
+        Me.LblUsername.AutoSize = True
+        Me.LblUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblUsername.Location = New System.Drawing.Point(38, 83)
+        Me.LblUsername.Name = "LblUsername"
+        Me.LblUsername.Size = New System.Drawing.Size(102, 25)
+        Me.LblUsername.TabIndex = 4
+        Me.LblUsername.Text = "Username"
+        '
+        'LblPIN
+        '
+        Me.LblPIN.AutoSize = True
+        Me.LblPIN.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblPIN.Location = New System.Drawing.Point(38, 113)
+        Me.LblPIN.Name = "LblPIN"
+        Me.LblPIN.Size = New System.Drawing.Size(54, 25)
+        Me.LblPIN.TabIndex = 5
+        Me.LblPIN.Text = "PINs"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(711, 360)
+        Me.Controls.Add(Me.LblPIN)
+        Me.Controls.Add(Me.LblUsername)
+        Me.Controls.Add(Me.LblSignup)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Controls.Add(Me.TxtPIN)
+        Me.Controls.Add(Me.TxtUsername)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtUsername As TextBox
+    Friend WithEvents TxtPIN As TextBox
+    Friend WithEvents BtnLogin As Button
+    Friend WithEvents LblSignup As Label
+    Friend WithEvents LblUsername As Label
+    Friend WithEvents LblPIN As Label
+End Class
--- textfiletopdf.vb
+++ textfiletopdf.vb
@@ -28,22 +28,14 @@ Public Class textfiletopdf
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
         Dim summaryMessage As String = "Transaction Summary:"
-
-        ' Gather information from relevant tabs:
-        Dim withdrawAmount As Decimal = 0 ' Example, replace with actual data retrieval
-        Dim depositAmount As Decimal = 0 ' Example, replace with actual data retrieval
-        ' ... (Retrieve data from other tabs as needed)
-
-        ' Construct the summary message:
+        Dim withdrawAmount As Decimal = 0
+        Dim depositAmount As Decimal = 0
         If withdrawAmount > 0 Then
             summaryMessage &= vbCrLf & "Withdrawal: " & withdrawAmount.ToString("C2")
         End If
         If depositAmount > 0 Then
             summaryMessage &= vbCrLf & "Deposit: " & depositAmount.ToString("C2")
         End If
-        ' ... (Add information from other tabs as needed)
-
-        ' Display the message box:
         Dim result As DialogResult = MessageBox.Show(summaryMessage, "Logout Summary", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
 
         If result = Windows.Forms.DialogResult.OK Then
@@ -53,11 +45,9 @@ Public Class textfiletopdf
 
             If saveFileDialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
                 Try
-                    ' Write the summary to a text file
                     Using writer As New System.IO.StreamWriter(saveFileDialog.FileName)
                         writer.Write(summaryMessage)
                     End Using
-
                     MessageBox.Show("Transaction summary saved to text file.", "File Saved", MessageBoxButtons.OK, MessageBoxIcon.Information)
                 Catch ex As Exception
                     MessageBox.Show("An error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
@@ -68,60 +58,39 @@ Public Class textfiletopdf
 
     Private accountBalance As Decimal = 1000
     Private Sub BtnDeposit_Click(sender As Object, e As EventArgs) Handles BtnDeposit.Click
-        ' Get the deposit amount from the user input
         Dim depositAmount As Decimal
-
         If Decimal.TryParse(TxtDeposit.Text, depositAmount) AndAlso depositAmount > 0 Then
-            ' Update the account balance
             accountBalance += depositAmount
-
-            ' Display the updated balance in Ringgit Malaysia (RM)
             MessageBox.Show($"Deposit successful! New balance: RM {accountBalance:N2}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
-            ' Optionally, you can log the transaction or update the balance in a database
         Else
             MessageBox.Show("Invalid deposit amount. Please enter a valid positive number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        ' Retrieve values from text boxes
         Dim oldPin As String = TextBox1.Text
         Dim newPin As String = TextBox2.Text
         Dim repeatNewPin As String = TextBox3.Text
-
-        ' Check if old pin is correct (you should replace this with your own validation logic)
         If Not IsOldPinCorrect(oldPin) Then
             MessageBox.Show("Incorrect old pin. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Return
         End If
-
-        ' Check if the new pin and repeat new pin match
         If newPin <> repeatNewPin Then
             MessageBox.Show("New pin and repeated pin do not match. Please re-enter.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Return
         End If
-
-        ' Perform the pin change (you should replace this with your own logic)
         If ChangePinn(oldPin, newPin) Then
             MessageBox.Show("Pin changed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
-            ' You can add additional logic here, such as closing the form or resetting the text boxes
         Else
             MessageBox.Show("Error changing pin. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
     End Sub
 
     Private Function IsOldPinCorrect(oldPin As String) As Boolean
-        ' Implement your logic to check if the old pin is correct
-        ' For example, compare it with a stored value in a database
-        ' Return True if correct, False otherwise
         Return True
     End Function
 
     Private Function ChangePinn(oldPin As String, newPin As String) As Boolean
-        ' Implement your logic to change the pin
-        ' For example, update the pin in a database
-        ' Return True if pin change is successful, False otherwise
         Return True
     End Function
 
